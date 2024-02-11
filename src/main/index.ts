a5efc8c8fc2d2431fa9427db002003823b7737e3
Lukas Veiga <lukas.veiga@hotmail.com>
2024-02-11 20:10:49 -0300

    feat: Add use case error for empty rows from google sheet

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,7 +1,16 @@
+import { UsecaseError } from "../usecases/errors/usecase-error";
 import writeValuesSheet from "./factory";
 
 const initialize = async () => {
-  writeValuesSheet.writeResults();
+  try {
+    writeValuesSheet.writeResults();
+  } catch (e) {
+    if (e instanceof UsecaseError) {
+      console.error("Use case error: " + e.message);
+    } else {
+      console.error("Google Sheets API Error");
+    }
+  }
 };
 
 initialize();
